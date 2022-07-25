6465c03fc3e51b74a30087969e159a437328e163
KasparsVanags <kaspisw@gmail.com>
2022-07-26 00:36:36 +0300

    Finish arrays folder

--- csharp-basics/exercises/Arrays/Arrays/Exercise3/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise3/Program.cs
@@ -1,24 +1,16 @@
-﻿namespace Exercise3
+﻿using System;
+using System.Linq;
+
+namespace Exercise3
 {
     class Program
     {
-        //TODO: Write a C# program to calculate the average value of array elements.
         private static void Main(string[] args)
         {
             int[] numbers = {20, 30, 25, 35, -16, 60, -100};
-
-            /*
-            fixme - calculate sum of all array elements 
-            int sum = 0;
-            for (?){
-                
-            }
-            */
-
-            /*
-            fixme - calculate average value 
+            var average = numbers.AsQueryable().Average();
+            
             Console.WriteLine("Average value of the array elements is : " + average);
-            */
         }
     }
 }
---
+++ csharp-basics/exercises/Arrays/Arrays/Exercise6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise6
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var rand = new Random();
+            int[] arr = new int[10];
+            int[] copyArr = new int[10];
+            const int maxNumber = 100;
+            const int numToEnter = -7;
+            
+            for (var i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(maxNumber);
+            }
+            
+            Array.Copy(arr, copyArr, arr.Length);
+            arr[arr.Length - 1] = numToEnter;
+            Console.WriteLine(string.Join(" ", arr));
+            Console.WriteLine(string.Join(" ", copyArr));
+        }
+    }
+}
